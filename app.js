49276898136d5e6a5b50f4007d30757f8ea3892a
Derrick Hinkle <derrick@pristine.io>
2014-02-13 23:48:10 -0600

    meh

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var request = require('request');
 var argv = require('yargs').argv;
 var cheerio = require('cheerio');
 var fs = require('fs');
+var express = require('express');
+var app = express();
 
 var pre;
 var suf;
@@ -15,32 +17,58 @@ if(!fs.existsSync('output/')){
 }
 
 if(argv.url){
-	var url = argv.url.split('{}');
-	console.log(url);
-	if(url.length >= 2){
-		pre = url[0];
-		suf = url[1];
-		doPull();
-	}else{
-		console.log('Bad URL format darling, give it another go...');
-		process.exit(1);
-	}
+	mass(argv.url, argv.max, argv.name, argv.id);
 }else{
-	console.log('Oops! Please give me a URL! Leaving...');
-	process.exit(1);
+	console.log('No url argument, defaulting to api mode...');
+	
+	app.all('*', function(req, res, next){
+		
+		res.header('Access-Control-Allow-Origin', '*');
+    	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    	res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    	next();
+	});
+
+	app.get('/v1/mass', function(req, res){
+		console.log('Got a call..');
+		console.log(req.query);
+
+		mass(req.query.url, req.query.max, req.query.name, req.query.selector, function(success, error){
+			
+		});
+		res.send(200);
+	});
+
+	app.listen(9001);
+	console.log('API Interface up on 9001');
 }
 
-function doPull(){
-
-	var max = 100;
-
-	if(argv.max){
-		max = argv.max;
+function mass(url, max, name, selector, callback){
+	if(url){
+		url = url.split('{}');
+		//console.log(url);
+		if(url.length >= 2){
+			pre = url[0];
+			suf = url[1];
+		}else{
+			console.log('Bad URL format darling, give it another go...');
+			if(argv.url){
+				process.exit(1);
+			}
+		}
+	}else{
+		console.log('Oops! Please give me a URL! Leaving...');
+		if(argv.url){
+			process.exit(1);
+		}
 	}
 
-	console.log(argv);
+	if(!max){
+		max = 50;
+	}
 
-	if(argv.name){
+	if(name){
 		if(!fs.existsSync('output/'+argv.name)){
 			fs.mkdirSync('output/'+argv.name);
 		}
@@ -48,7 +76,7 @@ function doPull(){
 
 	for(var i = 1; i<max; i++){
 		//console.log('Now trying to get: ' + pre+i+suf);
-		pullImgSrc(pre+i+suf, argv.id, i, grabImage);
+		pullImgSrc(pre+i+suf, selector, i, grabImage, callback);
 	}
 }
 
@@ -68,6 +96,7 @@ function pullImgSrc(_url, id, count, callback){
 			var $ = cheerio.load(body);
 			callback($(id).attr('src'), count);
 		}else{
+			callback(false)
 		}
 	});
 }
